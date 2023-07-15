97faab38de7073bdfbb55080b163f3f4849b0e0d
lbw33 <71776468+lbw33@users.noreply.github.com>
2023-07-15 16:43:50 +0100

    Check for expected CREATE2 address
    - Logic added to check predetermined address and whether that matches the expected deterministic address. Only lets deploy and verify if these two addresses match.

--- deploy/001_deploy_ew_wallet_factory.ts
+++ deploy/001_deploy_ew_wallet_factory.ts
@@ -4,27 +4,41 @@ import { DeployFunction } from 'hardhat-deploy/types';
 const deployEtherspotWalletFactory: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ) {
+  const EXPECTED_CREATE2_ADDRESS = '0x27f11918740060bd9Be146086F6836e18eedBB8C';
   const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+  const { deploy, deterministic } = deployments;
   const { from } = await getNamedAccounts();
 
   console.log('starting deployment of wallet factory...');
 
-  const ret = await deploy('EtherspotWalletFactory', {
+  const determined = await deterministic('EtherspotWalletFactory', {
     from,
     args: [],
-    gasLimit: 6e6,
-    // gasLimit: 1000000000, // arbitrum
     log: true,
-    deterministicDeployment: true,
   });
-  console.log('EtherspotWalletFactory deployed at:', ret.address);
 
-  await hre.run('verify:verify', {
-    address: ret.address,
-    contract: 'src/wallet/EtherspotWalletFactory.sol:EtherspotWalletFactory',
-    constructorArguments: [],
-  });
+  console.log(`Expected address: ${EXPECTED_CREATE2_ADDRESS}`);
+  console.log(`Pre-determined address: ${determined.address}`);
+
+  if (determined.address !== EXPECTED_CREATE2_ADDRESS) {
+    console.log('Pre-detemined address is different to what is expected!');
+  } else {
+    const ret = await deploy('EtherspotWalletFactory', {
+      from,
+      args: [],
+      gasLimit: 6e6,
+      // gasLimit: 1000000000, // arbitrum
+      log: true,
+      deterministicDeployment: true,
+    });
+    console.log('EtherspotWalletFactory deployed at:', ret.address);
+
+    await hre.run('verify:verify', {
+      address: ret.address,
+      contract: 'src/wallet/EtherspotWalletFactory.sol:EtherspotWalletFactory',
+      constructorArguments: [],
+    });
+  }
 };
 
 deployEtherspotWalletFactory.tags = [
