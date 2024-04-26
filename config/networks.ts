53c3a32570c841eb404ec60de608d7d7f7541949
Luke Wickens <lukewickens1989@gmail.com>
2024-04-26 11:29:54 +0100

    Deployed to Amoy
    - added network and Etherscan configs for Polygon Amoy testnet.
    - deployed and verified EtherspotWalletV1.
    - added deployment info to `DEPLOYMENTS.md`.

--- config/networks.ts
+++ config/networks.ts
@@ -213,6 +213,11 @@ const networks: HardhatUserConfig['networks'] = {
     url: 'https://rpcv2-testnet.ancient8.gg/',
     accounts: [process.env.DEPLOYER_PRIVATE_KEY!],
   },
+  amoy: {
+    chainId: 80002,
+    url: 'https://rpc-amoy.polygon.technology',
+    accounts: [process.env.DEPLOYER_PRIVATE_KEY!],
+  },
   dev: { url: 'http://localhost:8545' },
 };
 export default networks;
