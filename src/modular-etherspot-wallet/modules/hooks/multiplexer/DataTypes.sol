d2280660507b4a3f25037aa080455055f0f0f40c
kanth <kanth.explorer@gmail.com>
2024-10-01 06:42:23 +0530

    feat: HookMultiplexer

---
+++ src/modular-etherspot-wallet/modules/hooks/multiplexer/DataTypes.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+// Hook types
+enum HookType {
+    GLOBAL,
+    DELEGATECALL,
+    VALUE,
+    SIG,
+    TARGET_SIG
+}
+
+// Hook initialization data for sig hooks
+struct SigHookInit {
+    bytes4 sig;
+    address[] subHooks;
+}
+
+struct HookAndContext {
+    address hook;
+    bytes context;
+}
+
+struct SignatureHooks {
+    bytes4[] allSigs;
+    mapping(bytes4 => address[]) sigHooks;
+}
+
+struct Config {
+    bool initialized;
+    mapping(HookType hookType => address[]) hooks;
+    mapping(HookType hookType => SignatureHooks) sigHooks;
+}
