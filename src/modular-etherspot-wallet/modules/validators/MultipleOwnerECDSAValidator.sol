161afa934b11f3d4bfac05d246940edc48010c86
lbw33 <71776468+lbw33@users.noreply.github.com>
2024-12-10 09:53:37 +0000

    `MultipleOwnerECDSAValidator`: ERC-6492 support
    - Added extra validation for already signed messages in `isValidSignatureWithSender` to provide support for ERC-6492.
    - Create new deployment script specifically for `MultipleOwnerECDSAValidator`.

--- src/modular-etherspot-wallet/modules/validators/MultipleOwnerECDSAValidator.sol
+++ src/modular-etherspot-wallet/modules/validators/MultipleOwnerECDSAValidator.sol
@@ -75,6 +75,12 @@ contract MultipleOwnerECDSAValidator is IValidator {
         bytes32 hash,
         bytes calldata data
     ) external view override returns (bytes4) {
+        address recoveredOwner = ECDSA.recover(hash, data);
+        if (
+            ModularEtherspotWallet(payable(msg.sender)).isOwner(recoveredOwner)
+        ) {
+            return 0x1626ba7e; // ERC1271_MAGICVALUE
+        }
         bytes32 ethHash = ECDSA.toEthSignedMessageHash(hash);
         address owner = ECDSA.recover(ethHash, data);
         if (ModularEtherspotWallet(payable(msg.sender)).isOwner(owner)) {
